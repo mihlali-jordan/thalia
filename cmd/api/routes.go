a9cb1859c1da0a3aa2a9d46b8bf89115eea692a4
mihlali-jordan <biz@jordanmihlali.dev>
2024-09-08 14:47:02 +0200

    feat: create routes file

---
+++ cmd/api/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/go-chi/chi/v5"
+
+func (app *application) routes() *chi.Mux {
+	router := chi.NewRouter()
+	router.Route("/v1", func(r chi.Router) {
+		r.Get("/healthcheck", app.healthcheckHandler)
+		r.Route("/movies", func(r chi.Router) {
+			r.Post("/", app.createMovieHandler)
+			r.Get("/{id}", app.getMovieHandler)
+		})
+	})
+
+	return router
+}
