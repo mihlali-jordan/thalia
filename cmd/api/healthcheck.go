d0b53cf798eb0f0ddd19e646a3a94da85d5edac1
mihlali-jordan <biz@jordanmihlali.dev>
2024-09-08 15:34:04 +0200

    feat: implement json marshalling helper

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,29 +1,20 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
 // Implemented as a method on the application struct
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	data := map[string]string{
-		"status": "available",
+		"status":      "available",
 		"environment": app.config.env,
-		"version": version
+		"version":     version,
 	}
 
-	js, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-		return
 	}
-
-	js = append(js, "\n")
-
-	w.Header().Set("Content-Type", "application/json")
-
-	w.Write([]byte(js))
 }
