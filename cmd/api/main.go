de7f0fed3c5f29a3efe9fdb287c4204778067540
mihlali-jordan <biz@jordanmihlali.dev>
2024-09-08 17:40:48 +0200

    feat: append localhost and still use dynamic port

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -40,8 +41,7 @@ func main() {
 	r.Get("/v1/healthcheck", app.healthcheckHandler)
 
 	srv := &http.Server{
-		// Addr:         fmt.Sprintf(":%d", cfg.port),
-		Addr:         "localhost:4000",
+		Addr:         fmt.Sprintf("localhost:%d", cfg.port),
 		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
