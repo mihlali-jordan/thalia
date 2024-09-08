d23c9c5a6d071aabd5515ec17db38337218c73c3
mihlali-jordan <biz@jordanmihlali.dev>
2024-09-08 19:04:55 +0200

    feat: implement custom json decoding for runtime field

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("Invalid runtime format")
 
 type Runtime int32
 
@@ -12,4 +16,31 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+// Unmarshal method that satisfies the json.Unmashaler interface
+// Use a pointer because UnmarshalJSON() needs to modify the receiver or we will be
+// modifying a copy which gets discarded when the method returns
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Convert int32 to runtime type and assign it to receiver
+	// Use * to dereference the receiver in order to set underlying value of the pointer
+	*r = Runtime(i)
+
+	return nil
 }
