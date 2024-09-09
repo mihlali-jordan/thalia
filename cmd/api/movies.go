4dcb9e23e6bbf7f12751d49f9250ab4386afb4d4
mihlali-jordan <biz@jordanmihlali.dev>
2024-09-09 10:29:34 +0200

    feat: add partial updates

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -72,9 +72,9 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
+		Title   *string  `json:"title"`
+		Year    *int32   `json:"year"`
+		Runtime *int32   `json:"runtime"`
 		Genres  []string `json:"genres"`
 	}
 
@@ -84,10 +84,19 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
+
 	movie.ID = id
 
 	v := validator.New()
@@ -98,6 +107,12 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	err = app.models.Movies.Update(movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,7 +12,7 @@ func (app *application) routes() *chi.Mux {
 		r.Route("/movies", func(r chi.Router) {
 			r.Post("/", app.createMovieHandler)
 			r.Get("/{id}", app.getMovieHandler)
-			r.Put("/{id}", app.updateMovieHandler)
+			r.Patch("/{id}", app.updateMovieHandler)
 			r.Delete("/{id}", app.deleteMovieHandler)
 		})
 	})
